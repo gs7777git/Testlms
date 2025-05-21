4a98e935df419e79143dca3e5638906157a33890
gs7777git <govindsaini355@gmail.com>
2025-05-21 21:29:42 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,17 +1,31 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyGSkfDfkhtluqeHaN-CpZ68z-RU_OhNzz_XD5UTTjlymX3YTkiYvfBoYmeIterQMIU/exec';
+
 function login() {
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
   const errorMsg = document.getElementById("errorMsg");
 
-  if (email === "admin@example.com" && password === "admin123") {
-    localStorage.setItem("crm_user", email);
-    window.location.href = "dashboard.html";
-  } else {
-    errorMsg.textContent = "Invalid credentials.";
-  }
-}
-
-function logout() {
-  localStorage.removeItem("crm_user");
-  window.location.href = "index.html";
+  fetch(scriptURL, {
+    method: "POST",
+    body: JSON.stringify({
+      type: "login",
+      email: email,
+      password: password
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(result => {
+      if (result.result === "success") {
+        localStorage.setItem("crm_user", email);
+        window.location.href = "dashboard.html";
+      } else {
+        errorMsg.textContent = result.message;
+      }
+    })
+    .catch(() => {
+      errorMsg.textContent = "Login failed. Please try again.";
+    });
 }
